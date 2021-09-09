2afd1499069fa658bad7e9c90c07086d5c010015
Nancy <nancy.badran@canada.ca>
2021-09-09 11:37:36 -0400

    Import Namespaces into i18n.ts

--- frontend/src/i18n.ts
+++ frontend/src/i18n.ts
@@ -1,26 +1,60 @@
-import { initReactI18next } from 'react-i18next';
-import Backend from 'i18next-http-backend';
-import LanguageDetector from 'i18next-browser-languagedetector';
-import XHR from 'i18next-http-backend';
-import i18n from 'i18next';
+
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import i18n from "i18next";
+
+//english namespaces
+import enArtifacts from "./locales/en/artifacts.json";
+import enCommon from "./locales/en/common.json";
+import enExecutions from "./locales/en/executions.json";
+import enExperiments from "./locales/en/experiments.json";
+import enHome from "./locales/en/home.json";
+import enPipelines from "./locales/en/pipelines.json";
+
+//french namespaces
+import frArtifacts from "./locales/fr/artifacts.json";
+import frCommon from "./locales/fr/common.json";
+import frExecutions from "./locales/fr/executions.json";
+import frExperiments from "./locales/fr/experiments.json";
+import frHome from "./locales/fr/home.json";
+import frPipelines from "./locales/fr/pipelines.json";
+
+const resources = {
+  en: {
+    artifacts: enArtifacts,
+    common: enCommon,
+    executions: enExecutions,
+    experiments: enExperiments,
+    home: enHome,
+    pipelines: enPipelines,
+  },
+  fr: {
+    artifacts: frArtifacts,
+    common: frCommon,
+    executions: frExecutions,
+    experiments: frExperiments,
+    home: frHome,
+    pipelines: frPipelines,
+  },
+};
 
 const options = {
-  order: ['navigator'],
+  order: ["navigator"],
 };
+
 i18n
-  .use(XHR)
-  .use(Backend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    ns: ['common', 'home', 'artifacts', 'executions', 'experiments', 'pipelines'],
-    defaultNS: 'common',
-    fallbackLng: ['en', 'fr'],
+    detection: options,
+    resources,
+    fallbackLng: ["en", "fr"],
+    nonExplicitSupportedLngs: true,
     debug: true,
+    keySeparator: false,
     interpolation: {
       escapeValue: false,
     },
-    detection: options,
   });
 
 export default i18n;
